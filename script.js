c84d2cc074080410768c4f2d3fbeb32ebe162a0a
owencasabuena <owencasabuena@gmail.com>
2025-05-23 09:55:25 +0800

    UI added

--- script.js
+++ script.js
@@ -1,123 +1,106 @@
-function getHumanChoice()
-{
-    let humanChoice = prompt("Pick one (rock, paper, scissor): ");
-    humanChoice = humanChoice.toLowerCase();
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-    if (humanChoice === "rock")
-    {
-        return humanChoice;
+let win = 0;
+let draw = 0;
+let lose = 0;
+
+function determineResult(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        draw++;
+        return "DRAW!";
     }
-    else if (humanChoice === "paper")
-    {
-        return humanChoice;
-    }
-    else if (humanChoice === "scissor")
-    {
-        return humanChoice;
-    }
-    else 
-    {
-        console.error("Invalid Input");
+    else if((playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissors" && computerSelection == "paper")) {
+                win++;
+                return "YOU WIN!";
+            }
+    else {
+        lose++;
+        return "YOU LOSE...";
     }
 }
 
-function getComputerChoice() 
-{
-    let randomNumber = Math.floor((Math.random() * 3) + 1);
-    let computerChoice;
-
-    switch(randomNumber)
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1;
+    switch(random)
     {
         case 1:
-            computerChoice = "rock";
-            return computerChoice;
-            break;
+            return "rock";
         case 2:
-            computerChoice = "scissor";
-            return computerChoice;
-            break;
+            return "paper";
         case 3:
-            computerChoice = "paper";
-            return computerChoice;
-            break;
+            return "scissors";
     }
 }
 
-function playRound(humanChoice, computerChoice)
-{
-    let result;
-    let winResult = "You win! " + humanChoice + " beats " + computerChoice;
-    let loseResult = "You lose! " + computerChoice + " beats " + humanChoice;
+rock.addEventListener('click', function() {
+    startBattle("rock");
+});
 
-    if (humanChoice === "rock" && computerChoice === "paper")
-    {
-        result = loseResult;
-        computerScore++;
-        console.log(result);
-    }
-    else if (humanChoice === "rock" && computerChoice === "scissor")
-    {
-        result = winResult;
-        humanScore++;
-        console.log(result);
-    }
-    else if (humanChoice === "paper" && computerChoice === "rock")
-    {
-        result = winResult;
-        humanScore++;
-        console.log(result);
-    }
-    else if (humanChoice === "paper" && computerChoice === "scissor")
-    {
-        result = loseResult;
-        computerScore++;
-        console.log(result);
-    }
-    else if (humanChoice === "scissor" && computerChoice === "rock")
-    {
-        result = loseResult;
-        computerScore++;
-        console.log(result);
-    }
-    else if (humanChoice === "scissor" && computerChoice === "paper")
-    {
-        result = winResult;
-        humanScore++;
-        console.log(result);
-    }
-    else 
-    {
-        result = "It's a tie!";
-        console.log(result);
-    }
+paper.addEventListener('click', function() {
+    startBattle("paper");
+});
+
+scissors.addEventListener('click', function() {
+    startBattle("scissors");
+});
+
+function updateScore() {
+    const winScore = document.querySelector("#win");
+    const drawScore = document.querySelector("#draw");
+    const loseScore = document.querySelector("#lose");
+
+    winScore.textContent = `Win: ${win}`;
+    drawScore.textContent = `Draw: ${draw}`;
+    loseScore.textContent = `Lose: ${lose}`;
 }
 
-function playGame()
-{
-    for (let i = 0; i < 5; i++)
-    {
-        const computerSelection = getComputerChoice();
-        const humanSelection = getHumanChoice();
-        playRound(humanSelection, computerSelection);
+const replayButton = document.querySelector(".replay-button");
+replayButton.addEventListener('click', function() {
+    document.querySelector(".player-choice").style.display = 'block';
+    document.querySelector(".battle-area").style.display = 'none';
+    document.querySelector(".result-overlay").style.display = 'none';
+    document.querySelector(".player-pick").innerHTML = "";
+    document.querySelector(".computer-pick").innerHTML = "";
+    replayButton.style.display = 'none';
+});
 
-        if (i == 4)
-        {
-            console.log("Human Score: " + humanScore);
-            console.log("Computer Score: " + computerScore);
-    
-            if (humanScore < computerScore)
-            {
-                console.log("You lose");
-            }
-            else 
-            {
-                console.log("You win!")
-            }
-        }
+function startBattle(playerChoice) {
+    let computerChoice = getComputerChoice();
+    document.querySelector(".player-choice").style.display = 'none';
+    document.querySelector(".battle-area").style.display = 'flex';
+    let playerPick = document.createElement("img");
+    if(playerChoice === "rock") {
+        playerPick.setAttribute('src', 'https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/rock.png');
     }
+    else if(playerChoice === "paper") {
+        playerPick.setAttribute('src', 'https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/paper.png');
+    }
+    else {
+        playerPick.setAttribute('src', 'https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/scissors.png');
+    }
+    document.querySelector(".player-pick").appendChild(playerPick);
+
+    let computerPick = document.createElement("img");
+    if(computerChoice === "rock") {
+        computerPick.setAttribute('src', 'https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/rock.png');
+    }
+    else if(computerChoice === "paper") {
+        computerPick.setAttribute('src', 'https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/paper.png');
+    }
+    else {
+        computerPick.setAttribute('src', 'https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/scissors.png');
+    }
+    document.querySelector(".computer-pick").appendChild(computerPick);
+
+    let result = determineResult(playerChoice, computerChoice);
+    const resultOverlay = document.querySelector(".result-overlay");
+    resultOverlay.textContent = result;
+    resultOverlay.style.display = 'flex';
+
+    updateScore();
+    replayButton.style.display = "block";
 }
-
-let humanScore = 0;
-let computerScore = 0;
-
-playGame();
